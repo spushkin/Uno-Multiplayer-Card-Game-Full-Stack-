942f7a52f32e9459bfb1719b40b959aa7f41caf4
spushkin <pushkin.s89@gmail.com>
2024-05-19 21:44:58 -0700

    add global chat

--- uno-team-k-spring-2024/public/javascripts/game.js
+++ uno-team-k-spring-2024/public/javascripts/game.js
@@ -1,4 +1,5 @@
-const gameSocket = io();
+const socket2 = io();
+const messageField1 = document.querySelector("#message-field");
 
 // Retrieve userId from the hidden span
 const gameId = parseInt(document.querySelector("#gameIdSpan").innerText);
@@ -6,103 +7,103 @@ const ownSeat = parseInt(document.querySelector("#seatSpan").innerText);
 const userId = parseInt(document.querySelector("#userIdSpan").innerText); // Newly added line
 
 const playCard = async (id, color) => {
-  const colorElem = document.querySelector("#color");
+	const colorElem = document.querySelector("#color");
 
-  await fetch(`/play/${gameId}`, {
-    method: "post",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      action: "playCard",
-      card: id,
-      color: colorElem.value,
-    }),
-  });
+	await fetch(`/play/${gameId}`, {
+		method: "post",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({
+			action: "playCard",
+			card: id,
+			color: colorElem.value,
+		}),
+	});
 };
 
 gameSocket.on(`setPlayerCards:${gameId}`, ({ seat, cards }) => {
-  if (seat !== ownSeat) {
-    const board = document.querySelector("#board");
-    const oldSeat = document.querySelector(`#seat-${seat}`);
+	if (seat !== ownSeat) {
+		const board = document.querySelector("#board");
+		const oldSeat = document.querySelector(`#seat-${seat}`);
 
-    const seatEl = oldSeat || document.createElement("p");
-    seatEl.id = `seat-${seat}`;
-    seatEl.innerHTML = `Seat ${seat} has ${cards.length} cards`;
-    seatEl.innerHTML = `
+		const seatEl = oldSeat || document.createElement("p");
+		seatEl.id = `seat-${seat}`;
+		seatEl.innerHTML = `Seat ${seat} has ${cards.length} cards`;
+		seatEl.innerHTML = `
       <div class="seatInfo alert alert-warning">
         <p>Seat <b>${seat}</b> has <b>${cards.length}</b> cards</p>
       </div>`;
 
-    board.appendChild(seatEl);
-  } else {
-    const selfCards = document.querySelector("#selfCards");
-    selfCards.innerHTML = "";
+		board.appendChild(seatEl);
+	} else {
+		const selfCards = document.querySelector("#selfCards");
+		selfCards.innerHTML = "";
 
-    cards.forEach((card) => {
-      const cardEl = document.createElement("button");
-      cardEl.classList.add(`btn-color-${card.color}`);
-      cardEl.classList.add("btn");
-      cardEl.classList.add("btn-secondary");
+		cards.forEach((card) => {
+			const cardEl = document.createElement("button");
+			cardEl.classList.add(`btn-color-${card.color}`);
+			cardEl.classList.add("btn");
+			cardEl.classList.add("btn-secondary");
 
-      const cardContent = document.createElement("p");
-      cardContent.classList.add(`type-${card.type}`);
-      cardContent.innerText = "";
-      cardEl.append(cardContent);
-      cardEl.addEventListener("click", async () => {
-        await playCard(card.id);
-      });
-      selfCards.appendChild(cardEl);
-    });
-  }
+			const cardContent = document.createElement("p");
+			cardContent.classList.add(`type-${card.type}`);
+			cardContent.innerText = "";
+			cardEl.append(cardContent);
+			cardEl.addEventListener("click", async () => {
+				await playCard(card.id);
+			});
+			selfCards.appendChild(cardEl);
+		});
+	}
 });
 
 gameSocket.on(`setTurnPlayer:${gameId}`, ({ seat }) => {
-  const selfTurn = seat === ownSeat;
+	const selfTurn = seat === ownSeat;
 
-  document.querySelector("#turnText").innerHTML = selfTurn
-    ? "<h1 id='turnGreen'>Your turn</h1>"
-    : "<h1 id='turnRed'>Not your turn</h1>";
-  const takeCardButton = document.querySelector("#takeCard");
-  takeCardButton.disabled = !selfTurn;
+	document.querySelector("#turnText").innerHTML = selfTurn
+		? "<h1 id='turnGreen'>Your turn</h1>"
+		: "<h1 id='turnRed'>Not your turn</h1>";
+	const takeCardButton = document.querySelector("#takeCard");
+	takeCardButton.disabled = !selfTurn;
 
-  const selfCards = document.querySelector("#selfCards");
-  selfCards.childNodes.forEach((child) => {
-    child.disabled = !selfTurn;
-  });
+	const selfCards = document.querySelector("#selfCards");
+	selfCards.childNodes.forEach((child) => {
+		child.disabled = !selfTurn;
+	});
 });
 
 document.querySelector("#takeCard").addEventListener("click", async () => {
-  await fetch(`/play/${gameId}`, {
-    method: "post",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      action: "takeCard",
-    }),
-  });
+	await fetch(`/play/${gameId}`, {
+		method: "post",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({
+			action: "takeCard",
+		}),
+	});
 });
 
 gameSocket.on(`endGame:${gameId}`, () => {
-  window.location.href = "/home";
+	window.location.href = "/home";
 });
 
 gameSocket.on(`setCurrentCard:${gameId}`, ({ card }) => {
-  const el = document.querySelector("#currentCard");
-  el.innerHTML = `<h2>Current card is:</h2> <div class="color-${card.color} type-${card.type}"></div>`;
+	const el = document.querySelector("#currentCard");
+	el.innerHTML = `<h2>Current card is:</h2> <div class="color-${card.color} type-${card.type}"></div>`;
 });
 
 // Add event listener to leave the game
 const leaveGameButton = document.querySelector("#leaveGameButton"); // Add an element with this ID
 if (leaveGameButton) {
-  leaveGameButton.addEventListener("click", () => {
-    // Emit the leaveGame event to the server, including gameId and userId
-    gameSocket.emit("leaveGame", { gameId, userId });
-    // Redirect to the home page or any other desired page
-    console.log("Byyyyyyyyye");
-    window.location.href = "/home";
-  });
+	leaveGameButton.addEventListener("click", () => {
+		// Emit the leaveGame event to the server, including gameId and userId
+		gameSocket.emit("leaveGame", { gameId, userId });
+		// Redirect to the home page or any other desired page
+		console.log("Byyyyyyyyye");
+		window.location.href = "/home";
+	});
 }
 
-if (messageField) {
-	messageField.addEventListener("keydown", (event) => {
+if (messageField1) {
+	messageField1.addEventListener("keydown", (event) => {
 		if (event.keyCode === 13) {
 			fetch(`/chat/${gameId}`, {
 				method: "post",
@@ -118,7 +119,7 @@ if (messageField) {
 
 	const messages = document.querySelector("#messages");
 
-	socket.on(`chat:${gameId}`, ({ sender, user, message, timestamp }) => {
+	socket2.on(`chat:${gameId}`, ({ sender, user, message, timestamp }) => {
 		const template = document.querySelector("#message");
 
 		const content = template.content.cloneNode(true);
